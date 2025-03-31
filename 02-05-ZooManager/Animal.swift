133794c12d665ec5958909bd03c3a24dd90fee24
Rylie Castell <rylie.castell@gmail.com>
2025-03-31 14:44:09 +0200

    add animal class

---
+++ 02-05-ZooManager/Animal.swift
@@ -0,0 +1,25 @@
+//
+//  Animal.swift
+//  02-05-ZooManager
+//
+//  Created by Rylie Castell on 31.03.25.
+//
+
+import Foundation
+
+// Aufgabe 1.2 Tier modellieren
+
+class Animal {
+    var name: String
+    var age: Double
+    var isEndangered: Bool = false
+    var species: String
+    
+    init(name: String, age: Double, species: String) {
+        self.name = name
+        self.age = age
+        self.species = species
+    }
+    
+    // Notiz: automatisch generierter initializer hat auch isEndangered mit rein gepackt, habe ich aber wieder raus gelöscht, weil es ja einen default Wert gibt und es dementsprechend nicht immer nötig ist diesen Wert bei Initialisierung festzulegen
+}
--- 02-05-ZooManager/main.swift
+++ 02-05-ZooManager/main.swift
@@ -5,5 +5,9 @@
 
 import Foundation
 
-print("Hello, World!")
+// Aufgabe 1.2 Tier modellieren
+
+let animal1: Animal = Animal(name: "Simba", age: 3.4, species: "Lion")
+let animal2: Animal = Animal(name: "Timon", age: 1.5, species: "Meerkat")
+let animal3: Animal = Animal(name: "Pumba", age: 4.7, species: "Warthog")
 
